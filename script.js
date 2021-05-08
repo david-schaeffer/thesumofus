afaf3c1352d301ed6f7a3c1b6a180efc2561d5c7
dbschaeffer29 <70391103+dbschaeffer29@users.noreply.github.com>
2021-05-08 17:43:19 -0500

    added scroll on click functionality and finalized layout

--- script.js
+++ script.js
@@ -1,2 +1,38 @@
 // Selectors
-const audioBtn = document.querySelector(".audio-btn");
+const scrollDownButton = document.querySelector(".hero-btn-wrapper");
+const audioButtons = document.querySelectorAll(".audio-btn");
+
+// Variables
+const poolAudio = new Audio("pool_audio.mp3");
+poolAudio.oncanplay = function() {
+  if (document.querySelector("#pool-audio-btn").checked) this.play();
+}
+
+// Functions
+function playAudio(e) {
+  const audioType = e.id;
+  const iconLabel = e.parentElement.childNodes[3].childNodes[1];
+  iconLabel.classList.toggle("fa-play");
+  iconLabel.classList.toggle("fa-pause");
+  if(e.checked) {
+    switch (audioType) {
+      case "pool-audio-btn":
+      poolAudio.play();
+      break;
+    }
+  } else {
+    switch (audioType) {
+      case "pool-audio-btn":
+        poolAudio.pause();
+        break;
+      default:
+
+    }
+  }
+}
+
+function scrollToDiv(divClass) {
+  $('html,body').animate({
+    scrollTop: $("."+divClass).offset().top
+ });
+}
